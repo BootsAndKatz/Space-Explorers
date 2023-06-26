dd2425d2bf2eb7aae2a52943e67aa0019eac5cbc
Jackson Turner <jacksonturner101@gmail.com>
2023-06-26 19:28:49 +0100

    Player Stats and Alien collision
    Player has three lives which decrease when the player comes in contact with an alien.
    Aliens have 'Alien' tag, Asteroids have 'Asteroid' tag, Player has 'Player' tag. Player now also has a rigid body but doesn't use gravity. This is to get the collision detection to work. And the sphere collider on the Aliens are now a trigger.

---
+++ SpaceGame/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    int health = 3;
+    int currentHealth;
+    void Start()
+    {
+        currentHealth = health;
+    }
+
+    void TakeDamage()
+    {
+        currentHealth--;
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(this.gameObject);
+    }
+
+    void Heal()
+    {
+        currentHealth++;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Alien"))
+        {
+            TakeDamage();
+            Debug.Log("PLayer HIT!");
+        }
+    }
+}
