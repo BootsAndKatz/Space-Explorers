2713eae0e4c0426d8feee73a516e42a962697fbc
Jackson Turner <jacksonturner101@gmail.com>
2023-06-29 20:52:57 +0100

    Laser Shooting visuals
    Added a 'Laser' object to player. Shooting now enables the mesh renderer for the laser object then after a brief delay disables itself.

--- SpaceGame/Assets/Scripts/PlayerShooting.cs
+++ SpaceGame/Assets/Scripts/PlayerShooting.cs
@@ -6,9 +6,13 @@ public class PlayerShooting : MonoBehaviour
 {
     public Transform shootPos;
     public int damage = 50;
+    public GameObject laser;
+    MeshRenderer mrLaser;
+    float next;
+    public float delay;
     void Start()
     {
-       
+        mrLaser = laser.GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
@@ -20,11 +24,24 @@ public class PlayerShooting : MonoBehaviour
             if (Physics.Raycast(shootPos.position, Vector3.forward, out hit, 20))
             {
                 Debug.Log("Raycast hit: " + hit.collider.gameObject.name);
+                
                 Debug.DrawLine(shootPos.position, shootPos.position + new Vector3(0, 0, 20), Color.red, 0.2f);
 
                 hit.collider.GetComponent<EnemyStats>().TakeDamage(damage);
+
+                
+
             }
 
+            mrLaser.enabled = true;
+            next = Time.time + delay;
+
+        }
+
+        if(Time.time > next)
+        {
+            mrLaser.enabled = false;
         }
     }
+
 }
